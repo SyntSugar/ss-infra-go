68bff6773920e4ec1b9c831176324535f6826afe
ccbond <1315275049@qq.com>
2024-01-05 04:27:11 +0800

    fix: add allow header

--- api/server/middleware/cros.go
+++ api/server/middleware/cros.go
@@ -20,7 +20,7 @@ func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
 		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, api_key")
 		c.Header("Access-Control-Allow-Methods", "POST, HEAD, PATCH, OPTIONS, GET, PUT")
 
 		if c.Request.Method == "OPTIONS" {
